1c39a398d217fd14e53b6cd9bfe7916c0787863a
Gena Pankratov <gpankratov95@mail.ru>
2021-01-24 18:10:30 +0300

    try StringConversions

--- src/main/java/com/LeogenWP/JavaCore/chapter18/StringConversions.java
+++ src/main/java/com/LeogenWP/JavaCore/chapter18/StringConversions.java
@@ -1,4 +1,11 @@
 package com.LeogenWP.JavaCore.chapter18;
 
 public class StringConversions {
+    public static void main(String args[]){
+        int num = 19648;
+        System.out.println("Число " + num + "в двоичной форме: " + Integer.toBinaryString(num));
+        System.out.println("Число " + num + "в восьмеричной форме: " + Integer.toOctalString(num));
+        System.out.println("Число " + num + "в шестнадцатеричной форме: " + Integer.toHexString(num));
+
+    }
 }
