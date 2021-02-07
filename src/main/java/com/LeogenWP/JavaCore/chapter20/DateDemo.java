055f3373787cf8a4135ecddab175f809cb630ca6
Gena Pankratov <gpankratov95@mail.ru>
2021-02-07 12:42:22 +0300

    Add DateDemo

---
+++ src/main/java/com/LeogenWP/JavaCore/chapter20/DateDemo.java
@@ -0,0 +1,13 @@
+package com.LeogenWP.JavaCore.chapter20;
+
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+
+        Long msec = date.getTime();
+        System.out.println(msec);
+    }
+}
