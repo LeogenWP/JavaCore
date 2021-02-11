15f78629d7b7f3e655a07c612354845ee2fdb80f
Gena Pankratov <gpankratov95@mail.ru>
2021-02-11 20:36:37 +0300

    Add FileReaderDemo

---
+++ src/main/java/com/LeogenWP/JavaCore/chapter21/FileReaderDemo.java
@@ -0,0 +1,17 @@
+package com.LeogenWP.JavaCore.chapter21;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+    public static void main(String[] args) {
+        try(FileReader fr = new FileReader("FileReaderDemo.java")){
+            int c;
+            while((c = fr.read()) != -1){
+                System.out.println((char) c);
+            }
+        }catch  (IOException e){
+            System.out.println("Exception :( " + e);
+        }
+    }
+}
