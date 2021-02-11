abad892e2a4ff260e3c1028dbacb4ebb2cc99d3e
Gena Pankratov <gpankratov95@mail.ru>
2021-02-12 00:46:03 +0300

    Add ConsoleDemo

---
+++ src/main/java/com/LeogenWP/JavaCore/chapter21/ConsoleDemo.java
@@ -0,0 +1,16 @@
+package com.LeogenWP.JavaCore.chapter21;
+
+import java.io.Console;
+
+public class ConsoleDemo {
+    public static void main(String[] args) {
+        String str;
+        Console con;
+
+        con = System.console();
+        if(con == null)
+            return;
+        str = con.readLine("Put string: ");
+        con.printf("You have written :  %s\n" , str);
+    }
+}
