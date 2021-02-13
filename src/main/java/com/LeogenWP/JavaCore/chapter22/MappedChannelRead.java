5e13ad8958faa7337b45d9a4ada56a951dc63e6a
Gena Pankratov <gpankratov95@mail.ru>
2021-02-13 14:52:12 +0300

    Add MappedChannelRead

---
+++ src/main/java/com/LeogenWP/JavaCore/chapter22/MappedChannelRead.java
@@ -0,0 +1,23 @@
+package com.LeogenWP.JavaCore.chapter22;
+
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ScatteringByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MappedChannelRead {
+    public static void main(String[] args) {
+        try(FileChannel fChan = (FileChannel) Files.newByteChannel(Paths.get("test.txt")))
+        {
+            long fSize = fChan.size();
+            MappedByteBuffer mBuf = fChan.map(FileChannel.MapMode.READ_ONLY,0,fSize);
+            for(int i =0; i < fSize; i++){
+                System.out.println((char)mBuf.get());
+                System.out.println();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
