bba34d50eb75b88959b5618cf3b6b455bd0df93f
Gena Pankratov <gpankratov95@mail.ru>
2021-02-13 14:17:44 +0300

    Add ExplicitChannelRead

---
+++ src/main/java/com/LeogenWP/JavaCore/chapter22/ExplicitChannelRead.java
@@ -0,0 +1,39 @@
+package com.LeogenWP.JavaCore.chapter22;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ExplicitChannelRead {
+
+    public static void main(String[] args) {
+        int count;
+        Path filepath = null;
+
+        try {
+            filepath = Paths.get("test.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+            //зачем return????
+            return;
+        }
+
+        try(SeekableByteChannel fChan = Files.newByteChannel(filepath)){
+            ByteBuffer mBuf = ByteBuffer.allocate(128);
+            do{
+                count = fChan.read(mBuf);
+                if(count != -1){
+                    mBuf.rewind();
+                    for(int i = 0; i < count; i++){
+                        System.out.println((char)mBuf.get());
+                    }
+                }
+
+            }while(count != -1);
+        }catch (Exception e){
+         e.printStackTrace();
+        }
+    }
+}
