fee048f1d4c73b04f0380c21a59573f6cdd40a60
Gena Pankratov <gpankratov95@mail.ru>
2021-03-20 15:44:09 +0300

    add BarDemo

---
+++ src/main/java/com/LeogenWP/JavaCore/chapter28/BarDemo.java
@@ -0,0 +1,48 @@
+package com.LeogenWP.JavaCore.chapter28;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class BarDemo {
+
+    public static void main(String[] args) {
+        CyclicBarrier cb = new CyclicBarrier(3, new BarAction());
+        System.out.println("Запуск потоков");
+
+        new MyThread2(cb,"A");
+        new MyThread2(cb,"B");
+        new MyThread2(cb,"C");
+    }
+
+}
+
+class MyThread2 implements Runnable {
+    CyclicBarrier cbar;
+    String name;
+    MyThread2(CyclicBarrier c, String n) {
+        cbar = c;
+        name = n;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name);
+        try {
+            cbar.await();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+ class BarAction implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Барьер достигнут");
+    }
+}
